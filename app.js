98de35cea29ba0787fa04b62d90f7989869b5406
Josh Penrod <josh.penrod@startribune.com>
2018-10-31 06:46:09 -0500

    Assignment 6.5 DOM1 updated

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+  const newToDoText = document.getElementById('newToDoText');
 
-  addToDoForm.addEventListener('submit', event => {
+  addToDoForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // get the text
@@ -15,24 +15,36 @@ function onReady() {
     // create a new input
     let checkbox = document.createElement('input');
 
+    // create a delete button
+    let deleteButton = document.createElement('button')
+
+    deleteButton.innerHTML = 'delete';
+
+
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
     // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
+    newLi.innerHTML = title;
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+    // attach deleteButton to the li
+    newLi.appendChild(deleteButton);
+
     //empty the input
     newToDoText.value = '';
 
+    deleteButton.addEventListener = function() {
+      newLi.parentNode.removeChild(newLi);
+    }
   });
 }
 
 window.onload = function() {
   onReady();
-};
+}
